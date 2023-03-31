a023acc05290cc153cd4eca72967fad484397687
wanghehan <wanghehan@xiaomi.com>
2023-03-31 17:03:57 +0800

    refactor(avartar): sel avartar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/hehan-wang/chatgpt-web/main/src/assets/avatar.jpg',
+      name: '【程序员鹤涵】',
+      description: '<a href="https://github.com/hehan-wang" class="text-blue-500" target="_blank" >微信:hehan4096</a>',
     },
   }
 }
