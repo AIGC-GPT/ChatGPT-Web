13d2e75b7b81f9bd2a03e3c413fa8442fa811f7d
Simon <112924447+AIGC-GPT@users.noreply.github.com>
2023-04-09 01:49:33 +0800

    Update 403.vue

--- src/icons/403.vue
+++ src/icons/403.vue
@@ -4,7 +4,7 @@
   <!--  </div> -->
   <div class="w-[300px] m-auto" style="text-align:center;">
     <p style="margin-top: 10px; line-height: 1.8; font-size: 13px;">
-      打开微信扫描二维码关注公众号：<span style="color: #E9405A; font-weight: bold;">AI研究</span>
+      微信扫描二维码关注公众号：<span style="color: #E9405A; font-weight: bold;">AI研究</span>
       回复：<span style="color: #e9415a; font-weight: bold; font-size: 17px; margin-bottom: 45px;">403</span>
       <br>即可获取<span style="color: #e9415a; font-weight: bold;"> 验证码 </span>解锁 🔓
     </p>
