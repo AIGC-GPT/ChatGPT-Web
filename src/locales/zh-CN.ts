7d5e381cddc2d8ba5d16f3f52216940556579eb8
Simon <112924447+AIGC-GPT@users.noreply.github.com>
2023-04-09 02:15:01 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -25,7 +25,7 @@ export default {
     success: '操作成功',
     failed: '操作失败',
     verify: '验证',
-    unauthorizedTips: '未经授权，请先进行验证。',
+    unauthorizedTips: '打开微信扫一扫',
   },
   chat: {
     newChatButton: '新建聊天',
